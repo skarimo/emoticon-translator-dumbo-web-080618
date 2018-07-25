56aacc164967d3bbd36d20cf12edc3313e9d6c2a
Sherzod Karimov <karimovj@gmail.com>
2018-07-25 19:06:03 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,9 @@ end
 def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
   result = emoticon_hash['get_emoticon'][emoticon]
-  
+    if result == nil
+      "sorry, that emoticon was not found"
+  end 
   result
   
 end
